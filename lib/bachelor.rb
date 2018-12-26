94a3174f44fb06181ad29f060794d003d8b2c3f1
Rene Ventura <venturar729@gmail.com>
2018-12-26 00:25:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,9 @@
 def get_first_name_of_season_winner(data, season)
   # code here
   season_contestants = data[season]
-  winner = season_contestants.select {|contestant| contestat["status"] = "Winner"}
+  winner = season_contestants.select {|contestant| contestant["status"] = "Winner"}
   winner_name = winner["name"].split(" ")
   winner_name[0]
-
 end
 
 def get_contestant_name(data, occupation)
